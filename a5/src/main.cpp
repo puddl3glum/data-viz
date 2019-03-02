98509bf350a91f3ad0181a4ca97bb6633c3af774
William Setzer <wjsetzer@uab.edu>
2019-03-02 16:27:51 -0600

    shit cpp

--- a5/src/main.cpp
+++ a5/src/main.cpp
@@ -3,31 +3,51 @@
 #include <unistd.h>
 // #include <sys/sysinfo.h>
 #include <stdint.h>
-#include <string.h>
-#include <math.h>
+// #include <string.h>
+// #include <math.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
+#include "vertex.h"
+#include "pixelmap.h"
+
+/*
 // typedefs
 typedef struct {
+  size_t len;
   double** vertices;
-  size_t len;
 } vertices_t;
+*/
 
+/*
 typedef struct {
-  size_t** lines;
   size_t len;
+  size_t** lines;
 } lines_t;
+*/
+
+/*
+typedef struct {
+  size_t width;
+  size_t height;
+  double** pixels;
+} pixelmap_t;
+*/
 
 // forward declarations
-void search(vertices_t*, double*, size_t**);
-void insertvertex(vertices_t*, double*, size_t**);
-void march(const uint8_t*, const size_t, const size_t, vertices_t*, lines_t*);
-void freevertices(vertices_t);
-void freelines(lines_t);
-void printvertices(vertices_t);
-void printlines(lines_t);
-void insertvertices(vertices_t*, lines_t*, double*, double*);
-void interpolate(vertices_t*, lines_t*);
+// void search(vertices_t*, double*, size_t**);
+// void insertvertex(vertices_t*, double*, size_t**);
+// void march(const uint8_t*, const size_t, const size_t, vertices_t*, lines_t*);
+// void freevertices(vertices_t);
+// void freelines(lines_t);
+// void printvertices(vertices_t);
+// void printlines(lines_t);
+// void insertvertices(vertices_t*, lines_t*, double*, double*);
+// void interpolate(vertices_t*, lines_t*);
 // double distance(double, double, double, double);
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
 
@@ -53,7 +73,7 @@ int main(int argc, char* argv[]) {
   }
 
    if (optind >= argc) {
-    fprintf(stderr, "Expected arguments after options\n");
+    cerr << "Expected arguments after options" << endl;;
     exit(EXIT_FAILURE);
   }
 
@@ -61,10 +81,15 @@ int main(int argc, char* argv[]) {
   const size_t height = strtoul(argv[1 + optind], NULL, 10);
 
   // calloc buffer
-  uint8_t* data = calloc(height * width, sizeof(uint8_t));
+  uint8_t* data = ( uint8_t* ) calloc(height * width, sizeof(uint8_t));
 
   fread(data, sizeof(uint8_t), height * width, infile);
   fclose(infile);
+
+  // pixelmap_t pixelmap;
+  auto pixelmap = Pixelmap::frombytes(data, width, height);
+
+  free(data);
  
   // apply threshold
   // # pragma omp parallel for num_threads(get_nprocs()) collapse(2)
@@ -77,38 +102,44 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  vertices_t vertices = {NULL, 0};
-  lines_t lines = {NULL, 0};
+  // vertices_t vertices = {0, NULL};
+  // lines_t lines = {0, NULL};
+  vector<Vertex> vertexvec;
 
-  march(data, width, height, &vertices, &lines);
+  // march(data, width, height, &vertices, &lines);
 
   // connect(&vertices);
 
-  interpolate(&vertices, &lines);
+  // interpolate(&vertices, &lines);
 
-  printvertices(vertices);
-  printlines(lines);
+  // printvertices(vertices);
+  // printlines(lines);
 
   // cleanup
-  free(data);
-  freevertices(vertices);
-  freelines(lines);
+  // free(data);
+  // freevertices(vertices);
+  // freelines(lines);
 }
 
+/*
 void printvertices(vertices_t vertices) {
   for (size_t x = 0; x < vertices.len; x++) {
     double* vertex = vertices.vertices[x];
     printf("v %f %f 0\n", vertex[0], vertex[1]);
   }
 }
+*/
 
+/*
 void printlines(lines_t lines) {
   for (size_t x = 0; x < lines.len; x++) {
     size_t* line = lines.lines[x];
     printf("l %d %d\n", line[0], line[1]);
   }
 }
+*/
 
+/*
 void freevertices(vertices_t vertices) {
   for (size_t x = 0; x < vertices.len; x++) {
     free(vertices.vertices[x]);
@@ -116,7 +147,9 @@ void freevertices(vertices_t vertices) {
 
   free(vertices.vertices);
 }
+*/
 
+/*
 void freelines(lines_t lines) {
   for (size_t x = 0; x < lines.len; x++) {
     free(lines.lines[x]);
@@ -124,7 +157,9 @@ void freelines(lines_t lines) {
 
   free(lines.lines);
 }
+*/
 
+/*
 void search(vertices_t* vertices, double vertex[], size_t** index) {
   // searches array for vertex pair
   // if found, value at index is set to index, else unchanged
@@ -136,12 +171,14 @@ void search(vertices_t* vertices, double vertex[], size_t** index) {
     if (avertex[0] == vertex[0] && avertex[1] == vertex[1]) {
       // index expected to be null
       // allocate for it and set
-      *index = calloc(1, sizeof(size_t));
+      *index = ( size_t* ) calloc(1, sizeof(size_t));
       **index = x;
     }
   }
 }
+*/
 
+/*
 void insertvertex(vertices_t* vertices, double vertex[], size_t** index) {
 
   search(vertices, vertex, index);
@@ -154,13 +191,13 @@ void insertvertex(vertices_t* vertices, double vertex[], size_t** index) {
     // printf("vertices: %d\n", vertices->len);
 
     double** temp = vertices->vertices;
-    vertices->vertices = calloc(vertices->len, sizeof(double*));
+    vertices->vertices = ( double** ) calloc(vertices->len, sizeof(double*));
     memcpy(vertices->vertices, temp, sizeof(double*) * (vertices->len - 1));
     free(temp);
     
     vertices->vertices[vertices->len - 1] = vertex;
 
-    *index = calloc(1, sizeof(size_t));
+    *index = ( size_t* ) calloc(1, sizeof(size_t));
     **index = vertices->len - 1;
   }
   else {
@@ -169,27 +206,31 @@ void insertvertex(vertices_t* vertices, double vertex[], size_t** index) {
     free(vertex);
   }
 }
+*/
 
+/*
 void insertline(lines_t* lines, size_t line[]) {
-  /*
+  *
    * Inserts a line into the last index
-   * */
+   * *
 
   lines->len++;
 
   size_t** temp = lines->lines;
-  lines->lines = calloc(lines->len, sizeof(size_t*));
+  lines->lines = ( size_t** ) calloc(lines->len, sizeof(size_t*));
   memcpy(lines->lines, temp, sizeof(size_t*) * (lines->len - 1));
   free(temp);
 
   lines->lines[lines->len - 1] = line;
 }
+*/
 
+/*
 void insertvertices(vertices_t* vertices, lines_t* lines, double* vertex0, double* vertex1) {
 
-  size_t** index = calloc(1, sizeof(size_t*));
+  size_t** index = ( size_t** ) calloc(1, sizeof(size_t*));
 
-  size_t* line = calloc(2, sizeof(size_t));
+  size_t* line = ( size_t* ) calloc(2, sizeof(size_t));
 
   *index = NULL;
   insertvertex(vertices, vertex0, index);
@@ -205,12 +246,14 @@ void insertvertices(vertices_t* vertices, lines_t* lines, double* vertex0, doubl
 
   free(index);
 }
+*/
 
+/*
 void march(const uint8_t* data, const size_t width, const size_t height, vertices_t* vertices, lines_t* lines) {
-  /*
+  *
    * Sets vertices and lines to vertices and lines pointers.
    * The values, if these pointers contain any, will be overwritten
-   * */
+   * *
 
   // returns the vertices and lines after marching
   // vertices are stored at vertices pointer (which will be clobbered)
@@ -261,11 +304,11 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
         case 0b0001: ; // blank statement after label /shrug
           // diagonal bottom right
           // one third from bottom on right
-          right = calloc(2, sizeof(double));
+          right = ( double* ) calloc(2, sizeof(double));
           right[0] = rc;
           right[1] = br - 0.5;
 
-          bottom = calloc(2, sizeof(double));
+          bottom = ( double* ) calloc(2, sizeof(double));
           bottom[0] = rc - 0.5;
           bottom[1] = br;
 
@@ -276,13 +319,13 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
         case 0b0010: ;
           // diagonal bottom left
           // one third from bottom on left
-          left = calloc(2, sizeof(double));
+          left = ( double*  ) calloc(2, sizeof(double));
           left[0] = lc;
           left[1] = br - 0.5;
 
           // bottom
           // one third from left on bottom
-          bottom = calloc(2, sizeof(double));
+          bottom = ( double* ) calloc(2, sizeof(double));
           bottom[0] = lc + 0.5;
           bottom[1] = br;
 
@@ -293,12 +336,12 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
         case 0b1000: ;
           // diagonal top left
           // one third from top on left
-          left = calloc(2, sizeof(double));
+          left = ( double* ) calloc(2, sizeof(double));
           left[0] = lc;
           left[1] = tr + 0.5;
 
           // one third from left on top
-          top = calloc(2, sizeof(double));
+          top = ( double* ) calloc(2, sizeof(double));
           top[0] = lc + 0.5;
           top[1] = tr;
 
@@ -309,12 +352,12 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
         case 0b0100: ;
           // diagonal top right
           // one third from top on right
-          right = calloc(2, sizeof(double));
+          right = ( double* ) calloc(2, sizeof(double));
           right[0] = rc;
           right[1] = tr + 0.5;
 
           // one third from right on top
-          top = calloc(2, sizeof(double));
+          top = ( double* ) calloc(2, sizeof(double));
           top[0] = rc - 0.5;
           top[1] = tr;
 
@@ -325,11 +368,11 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
         case 0b0011: ;
           // horizontal line
           // left halway from bottom
-          left = calloc(2, sizeof(double));
+          left = ( double* ) calloc(2, sizeof(double));
           left[0] = lc;
           left[1] = br - 0.5;
 
-          right = calloc(2, sizeof(double));
+          right = ( double* ) calloc(2, sizeof(double));
           right[0] = rc;
           right[1] = br - 0.5;
 
@@ -339,11 +382,11 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
         case 0b1010: ;
         case 0b0101: ;
           // vertical line
-          top = calloc(2, sizeof(double));
+          top = ( double* ) calloc(2, sizeof(double));
           top[0] = lc + 0.5;
           top[1] = tr;
 
-          bottom = calloc(2, sizeof(double));
+          bottom = ( double* ) calloc(2, sizeof(double));
           bottom[0] = lc + 0.5;
           bottom[1] = br;
 
@@ -353,12 +396,12 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
         case 0b0110: ;
           // diagonal top left
           // one third on top from left
-          top = calloc(2, sizeof(double));
+          top = ( double* ) calloc(2, sizeof(double));
           top[0] = lc + 0.5;
           top[1] = tr;
 
           // one third on left from top
-          left = calloc(2, sizeof(double));
+          left = ( double* ) calloc(2, sizeof(double));
           left[0] = lc;
           left[1] = tr + 0.5;
 
@@ -366,12 +409,12 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
 
           // diagonal bottom right
           // one third on bottom from right
-          bottom = calloc(2, sizeof(double));
+          bottom = ( double* ) calloc(2, sizeof(double));
           bottom[0] = rc - 0.5;
           bottom[1] = br;
 
           // one third on right from bottom
-          right = calloc(2, sizeof(double));
+          right = ( double* ) calloc(2, sizeof(double));
           right[0] = rc;
           right[1] = br - 0.5;
 
@@ -380,12 +423,12 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
         case 0b1001: ;
           // diagonal top right
           // one third from top on right
-          right = calloc(2, sizeof(double));
+          right = ( double* ) calloc(2, sizeof(double));
           right[0] = rc;
           right[1] = tr + 0.5;
 
           // one third from right on top
-          top = calloc(2, sizeof(double));
+          top = ( double* ) calloc(2, sizeof(double));
           top[0] = rc - 0.5;
           top[1] = tr;
 
@@ -393,13 +436,13 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
 
           // diagonal bottom left
           // one third from bottom on left
-          left = calloc(2, sizeof(double));
+          left = ( double* ) calloc(2, sizeof(double));
           left[0] = lc;
           left[1] = br - 0.5;
 
           // bottom
           // one third from left on bottom
-          bottom = calloc(2, sizeof(double));
+          bottom = ( double* ) calloc(2, sizeof(double));
           bottom[0] = lc + 0.5;
           bottom[1] = br;
 
@@ -414,19 +457,10 @@ void march(const uint8_t* data, const size_t width, const size_t height, vertice
     }
   }
 }
+*/
 
-double distance(double x0, double y0, double x1, double y1) {
 
-  double x = x1 - x0;
-  double y = y1 - y0;
-
-  x *= x;
-  y *= y;
-
-  return sqrt(x + y);
-
-}
-
+/*
 void interpolate(vertices_t* vertices, lines_t* lines) {
 
   if (lines->len == 0)
@@ -471,4 +505,5 @@ void interpolate(vertices_t* vertices, lines_t* lines) {
   // exit(0);
 
 }
+*/
 
---
+++ a5/src/vertex.cpp
@@ -0,0 +1,21 @@
+
+#include <string>
+
+#include "vertex.h"
+
+class Vertex {
+  private:
+    double x;
+    double y;
+  public:
+    Vector();
+    std::string string();
+};
+
+Vertex::Vertex() {
+
+}
+
+std::string Vertex::string() {
+  
+}
